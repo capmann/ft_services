23f765a253ebf3de3b9ec82e41f1c892ac9f8425
user42 <ljurdant@student.42.fr>
2021-02-28 23:23:12 +0100

    finito

--- setup.sh
+++ setup.sh
@@ -7,6 +7,8 @@ fi
 minikube delete
 minikube start --driver=docker
 minikube addons enable metallb
+minikube addons enable metrics-server
+minikube addons enable dashboard
 kubectl delete configmap config -n metallb-system
 kubectl apply -f ./wordpress/config.yaml
 
@@ -16,6 +18,9 @@ docker build -t wordpress ./wordpress/
 docker build -t mysql ./mysql/ 
 docker build -t phpmyadmin ./phpmyadmin/
 docker build -t ftps ./ftps/
+docker build -t grafana ./grafana/
+docker build -t influxdb ./influxdb/
+docker build -t telegraf ./telegraf/
 
 kubectl apply -f ./nginx/nginx_deployment.yaml
 kubectl apply -f ./nginx/nginx_service.yaml 
@@ -28,3 +33,12 @@ kubectl apply -f ./phpmyadmin/phpmyadmin_deployment.yaml
 kubectl apply -f ./phpmyadmin/phpmyadmin_service.yaml
 kubectl apply -f ./ftps/ftps_deployment.yaml
 kubectl apply -f ./ftps/ftps_service.yaml
+kubectl apply -f ./influxdb/pvc.yaml
+kubectl apply -f ./influxdb/influxdb_deployment.yaml
+kubectl apply -f ./influxdb/influxdb_service.yaml
+kubectl apply -f ./grafana/grafana_deployment.yaml
+kubectl apply -f ./grafana/grafana_service.yaml
+kubectl apply -f ./telegraf/telegraf-secret.yaml
+kubectl apply -f ./telegraf/telegraf-config.yaml
+kubectl apply -f ./telegraf/telegraf_deployment.yaml
+kubectl apply -f ./telegraf/telegraf_service.yaml
